9930f795a87fca05a27857a3cd03011309c49791
grit1life <grit1life@gmail.com>
2020-03-02 13:52:22 +0900

    <팀프로젝트> 템플릿

---
+++ teamproject/src/main/java/kr/or/ksmart/TeamprojectApplication.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectApplication.class, args);
+	}
+
+}
---
+++ teamproject/src/test/java/kr/or/ksmart/TeamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
