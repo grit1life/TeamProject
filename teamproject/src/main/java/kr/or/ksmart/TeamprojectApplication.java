834e3c111f1b114081fd2fed1d5f29c2be53dd0a
grit1life <grit1life@gmail.com>
2020-03-04 17:14:08 +0900

    초기설정
    log4j, application.property ..etc

--- teamproject/src/main/java/kr/or/ksmart/TeamprojectApplication.java
+++ teamproject/src/main/java/kr/or/ksmart/TeamprojectApplication.java
@@ -1,9 +1,12 @@
 package kr.or.ksmart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //db 연결시 삭제
 public class TeamprojectApplication {
 
 	public static void main(String[] args) {
---
+++ teamproject/src/main/java/kr/or/ksmart/config/WebConfig.java
@@ -0,0 +1,26 @@
+package kr.or.ksmart.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.or.ksmart.interceptor.CommonInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+		
+	@Autowired
+	private CommonInterceptor commonInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(commonInterceptor)
+		        //.addPathPatterns("/**")
+		        //.excludePathPatterns("/")
+		        //.excludePathPatterns("/css/**")
+				;
+		
+	}
+}
