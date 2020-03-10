36b1d7a24f9a08c7f108e36126c640e074cf4dda
m16-1531712 <59201939+m16-1531712@users.noreply.github.com>
2020-03-10 12:31:40 +0900

    [조용민]: front 불필요한 파일 삭제 및 앱서랍 사전작업

--- teamproject/src/main/java/kr/or/ksmart/controller/GoodsController.java
+++ teamproject/src/main/java/kr/or/ksmart/controller/GoodsController.java
@@ -1,8 +1,14 @@
 package kr.or.ksmart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GoodsController {
+	
+	@GetMapping("goodsList")
+	public String goodsList () {
+		return "goods/goodsList";
+	}
 
 }
