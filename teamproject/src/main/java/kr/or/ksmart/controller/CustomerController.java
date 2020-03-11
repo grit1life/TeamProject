0edcc256bf86dac4698597a578e7a5aa6374248a
wngus8879 <wngus8879@naver.com>
2020-03-11 15:56:52 +0900

    3월 10일, customer 등록(화면만), 리스트 구현 완료

--- teamproject/src/main/java/kr/or/ksmart/controller/CustomerController.java
+++ teamproject/src/main/java/kr/or/ksmart/controller/CustomerController.java
@@ -1,13 +1,18 @@
 package kr.or.ksmart.controller;
 
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import kr.or.ksmart.service.CustomerService;
+
 
 @Controller
 public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
 	
 	
 	//고객,거래처 등록
@@ -17,5 +22,12 @@ public class CustomerController {
 		
 		return "/customer/cinsert";
 	}
-
+	
+	//고객,거래처 리스트
+	@GetMapping("/clist")
+	public String CustomerList(Model model) {
+		model.addAttribute("CustomerList", customerService.getCustomerList());
+			
+		return "/customer/clist";
+	}
 }
