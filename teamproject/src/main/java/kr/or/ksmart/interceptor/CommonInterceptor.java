84b058af843fd9a619873902f389a6ca609e4a0d
grit1life <grit1life@gmail.com>
2020-03-12 16:04:44 +0900

    interceptor log4j 설정

--- teamproject/src/main/java/kr/or/ksmart/interceptor/CommonInterceptor.java
+++ teamproject/src/main/java/kr/or/ksmart/interceptor/CommonInterceptor.java
@@ -17,7 +17,6 @@ public class CommonInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
 		logger.info("CommonInterceptor==========================");
 		logger.info("ACCESS INFO ======================= START ");
 		logger.info("Port           :::::{} ", request.getLocalPort());
