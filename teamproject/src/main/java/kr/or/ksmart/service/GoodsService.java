1358f26ad09501a565c445624222b7c20bb97124
m16-1531712 <59201939+m16-1531712@users.noreply.github.com>
2020-03-12 14:19:07 +0900

    [조용민] head 파일에 제이쿼리 추가(개별 페이지에서는 삭제하세요)

--- teamproject/src/main/java/kr/or/ksmart/service/GoodsService.java
+++ teamproject/src/main/java/kr/or/ksmart/service/GoodsService.java
@@ -16,14 +16,15 @@ public class GoodsService {
 	@Autowired
 	private GoodsMapper goodsMapper;
 	
-	// 상품 전체 조회 처리
+	// 상품 전체 조회 서비스
 	public List<Goods> getGoodsList() {
 		return goodsMapper.getGoodsList();
 	}
 	
-	// 상품 개별 조회 처리
+	// 상품 개별 조회 서비스
 	public Goods getGoods() {
 		return goodsMapper.getGoods();
 	}
+	
 
 }
