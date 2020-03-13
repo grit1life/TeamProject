14595c90615332de5d41f75f2ad31842e9404e50
wngus8879 <wngus8879@naver.com>
2020-03-13 13:00:34 +0900

    3월 13일 - 수정

--- teamproject/src/main/java/kr/or/ksmart/controller/CustomerController.java
+++ teamproject/src/main/java/kr/or/ksmart/controller/CustomerController.java
@@ -17,7 +17,7 @@ public class CustomerController {
 	private CustomerService customerService;
 	
 	
-	//고객,거래처 등록
+	//개인 고객 등록
 	@GetMapping("/cinsert")
 	public String addCustomer() {
 		
@@ -25,13 +25,30 @@ public class CustomerController {
 		return "/customer/cinsert";
 	}
 	
-	//고객,거래처 리스트
+	//개인사업, 거래처 고객 등록
+	@GetMapping("/cinsert2")
+	public String addCustomer2() {
+			
+			
+		return "/customer/cinsert2";
+	}
+	
+	//개인고객 리스트
 	@GetMapping("/clist")
 	public String CustomerList(Model model) {
 		model.addAttribute("CustomerList", customerService.getCustomerList());
 			
 		return "/customer/clist";
 	}
+	
+	
+	//개인사업, 거래처 고객 리스트
+	@GetMapping("/clist2")
+	public String CustomerCompanyList(Model model) {
+			model.addAttribute("CustomerCompanyList", customerService.getCustomerList());
+				
+			return "/customer/clist2";
+		}
 	
 	//고객,거래처 상세보기
 	@GetMapping("/cupdate")
