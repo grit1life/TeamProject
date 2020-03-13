35183d812acbf1972e8072a5e81b0458a99fe814
wngus8879 <wngus8879@naver.com>
2020-03-13 15:03:35 +0900

    2020년 03월 13일 - 개인고객/ 개인사업자, 법인 고객 리스트 및 등록 구현(상세보기 화면만 구현)

--- teamproject/src/main/java/kr/or/ksmart/service/CustomerService.java
+++ teamproject/src/main/java/kr/or/ksmart/service/CustomerService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.ksmart.domain.Customer;
+import kr.or.ksmart.domain.CustomerCompany;
 import kr.or.ksmart.mapper.CustomerMapper;
+
 
 
 @Service
@@ -19,11 +21,27 @@ public class CustomerService {
 	@Autowired
 	private CustomerMapper customerMapper;
 	
+	
+	//개인 고객 등록 
+	
+	public int cInsert(Customer customer){
+		
+		return customerMapper.cInsert(customer);
+		
+	}
+	//개인 고객 리스트
 	public List<Customer> getCustomerList() {
 		return customerMapper.getCustomerList();
 
 	
 	}
+	
+	//개인 사업& 법인 거래처 고객 고객 리스트
+		public List<CustomerCompany> getCustomerCompanyList() {
+			return customerMapper.getCustomerCompanyList();
+
+		
+		}
 	
 	
 	
