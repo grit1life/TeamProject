d94bd726ae813cc722111497f881006dd8d7074b
m16-1531712 <59201939+m16-1531712@users.noreply.github.com>
2020-03-16 09:32:53 +0900

    [조용민] 최신화

--- teamproject/src/main/java/kr/or/ksmart/domain/Goods.java
+++ teamproject/src/main/java/kr/or/ksmart/domain/Goods.java
@@ -1,13 +1,18 @@
 package kr.or.ksmart.domain;
 
+
 public class Goods {
 	
-	// 상품 카테고리의 테이블의 내용도 추가
 	private String goodsCode;
 	private String goodsCategoryCode;
 	private String goodsName;
+	private String goodsBarcode;
 	private String goodsInputStaffCode;
 	private String goodsInputDate;
+	private String goodsUpdateStaffCode;
+	private String goodsUpdateDate;
+	private String goodsDesc;
+	private String goodsImg;
 	
 	public String getGoodsCode() {
 		return goodsCode;
@@ -27,6 +32,12 @@ public class Goods {
 	public void setGoodsName(String goodsName) {
 		this.goodsName = goodsName;
 	}
+	public String getGoodsBarcode() {
+		return goodsBarcode;
+	}
+	public void setGoodsBarcode(String goodsBarcode) {
+		this.goodsBarcode = goodsBarcode;
+	}
 	public String getGoodsInputStaffCode() {
 		return goodsInputStaffCode;
 	}
@@ -38,5 +49,29 @@ public class Goods {
 	}
 	public void setGoodsInputDate(String goodsInputDate) {
 		this.goodsInputDate = goodsInputDate;
+	}
+	public String getGoodsUpdateStaffCode() {
+		return goodsUpdateStaffCode;
+	}
+	public void setGoodsUpdateStaffCode(String goodsUpdateStaffCode) {
+		this.goodsUpdateStaffCode = goodsUpdateStaffCode;
+	}
+	public String getGoodsUpdateDate() {
+		return goodsUpdateDate;
+	}
+	public void setGoodsUpdateDate(String goodsUpdateDate) {
+		this.goodsUpdateDate = goodsUpdateDate;
+	}
+	public String getGoodsDesc() {
+		return goodsDesc;
+	}
+	public void setGoodsDesc(String goodsDesc) {
+		this.goodsDesc = goodsDesc;
+	}
+	public String getGoodsImg() {
+		return goodsImg;
+	}
+	public void setGoodsImg(String goodsImg) {
+		this.goodsImg = goodsImg;
 	}
 }
