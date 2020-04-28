064e9aaf9657f3c0ac01ccacf9646eccf895aaca
wngus8879 <wngus8879@naver.com>
2020-04-28 13:06:34 +0900

    4.28일 업로드

--- teamproject/src/main/java/kr/or/ksmart/controller/AsRepairFeeBillingAndDepositController.java
+++ teamproject/src/main/java/kr/or/ksmart/controller/AsRepairFeeBillingAndDepositController.java
@@ -1,9 +1,14 @@
 package kr.or.ksmart.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import kr.or.ksmart.domain.RepairFeeBillingAndDeposit;
 import kr.or.ksmart.service.RepairFeeBillingAndDepositService;
 
@@ -14,7 +19,18 @@ public class AsRepairFeeBillingAndDepositController {
 	@Autowired
 	private RepairFeeBillingAndDepositService RepairFeeBillingAndDepositService;
 	
+	//청구서
+	@GetMapping("/asRepairBillForm")
+	public String asRepairBillForm(){
+		
+		return "/asRepairFeeBillingAndDeposit/asRepairBillForm";
+		
+		
+		
+	}
 	
+	
+	//등록
 	@GetMapping("/repairFeeBillingAndDeposit")
 	public String repairFeeBillingAndDeposit(){
 		
@@ -31,16 +47,28 @@ public class AsRepairFeeBillingAndDepositController {
 		
 		return "/asRepair/repairFeeBillingAndDeposit";
 	}
-		
 	
+	
+	
+	//리스트
 	@GetMapping("/rdList")
-	public String rbList(){
+	public String pList(Model model, @RequestParam(value="page", required = false) String page) {
+		if(page==null) {
+			page = "1";
+		}
+		int pageNum = Integer.parseInt(page);
+		Map<String, Object> map = RepairFeeBillingAndDepositService.getrepairFeeBillingAndDepositList(pageNum);
+		
+		model.addAttribute("RepairFeeBillingAndDepositList", map.get("RepairFeeBillingAndDepositList"));
+		model.addAttribute("currentPage", map.get("currentPage"));
+		model.addAttribute("startPage", map.get("startPage"));
+		model.addAttribute("endPage", map.get("endPage"));
+		model.addAttribute("lastPage", map.get("lastPage"));
 		
 		return "/asRepairFeeBillingAndDeposit/rdList";
-		
-		
-		
 	}
+		
+	
 	
 	@GetMapping("/rdUpdate")
 	public String rdUpdate(){
--- teamproject/src/main/java/kr/or/ksmart/mapper/RepairFeeBillingAndDepositMapper.java
+++ teamproject/src/main/java/kr/or/ksmart/mapper/RepairFeeBillingAndDepositMapper.java
@@ -1,5 +1,7 @@
 package kr.or.ksmart.mapper;
 
+import java.util.List;
+
 import kr.or.ksmart.domain.RepairFeeBillingAndDeposit;
 
 public interface RepairFeeBillingAndDepositMapper {
@@ -10,5 +12,10 @@ public interface RepairFeeBillingAndDepositMapper {
 		public int getrepairFeeBillingAndDepositCodeMax();
 		//등록
 		public int rdInsert(RepairFeeBillingAndDeposit RepairFeeBillingAndDeposit);
+		//리스트 페이징 카운트
+		public int getrepairFeeBillingAndDepositCnt();		
+		//리스트
+		public List<RepairFeeBillingAndDeposit> getrepairFeeBillingAndDepositList(int startClmn, int endClmn);
+		
 
 }
